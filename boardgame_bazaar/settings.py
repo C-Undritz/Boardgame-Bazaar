817328109d066d5f4c64f0ec10fd37c974dc5fc2
C-Undritz <undritz.cu@gmail.com>
2021-08-19 21:26:59 +0000

    feat: added multibuy discount values

--- boardgame_bazaar/settings.py
+++ boardgame_bazaar/settings.py
@@ -154,6 +154,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+MULTIBUY_DISCOUNT_ONE = 3
+MULTIBUY_DISCOUNT_TWO = 5
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
