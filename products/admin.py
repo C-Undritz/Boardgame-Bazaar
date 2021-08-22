06218c8a1e34cac1dfd9eedd3610f84195b6e283
C-Undritz <undritz.cu@gmail.com>
2021-08-22 13:16:49 +0000

    maint: customized admin view

--- products/admin.py
+++ products/admin.py
@@ -2,8 +2,55 @@ from django.contrib import admin
 from .models import Product, Genre, Status, Condition, GenreAssignment
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Genre)
-admin.site.register(Status)
-admin.site.register(Condition)
-admin.site.register(GenreAssignment)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'product_name',
+        'status',
+        'condition',
+        'price',
+        'stock',
+        'status',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ConditionAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class GenreAssignmentAdmin(admin.ModelAdmin):
+    list_display = (
+        'Product',
+        'genre',
+    )
+
+    ordering = ('Product',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Condition, ConditionAdmin)
+admin.site.register(GenreAssignment, GenreAssignmentAdmin)
