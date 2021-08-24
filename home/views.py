28caeb7b666cbe8afbabccfc01eb9a7732baef76
C-Undritz <undritz.cu@gmail.com>
2021-08-24 16:26:35 +0000

    feat: added free text search and categories search

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from products.models import Product
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
+from products.models import Product, Category
 
 
 def index(request):
@@ -8,9 +10,29 @@ def index(request):
     includes sorting a searching queries.
     """
     products = Product.objects.all()
+    query = None
+    category = None
+
+    # Search query function from Boutique Ado walkthrough project
+    if request.GET:
+        if 'category' in request.GET:
+            category = request.GET['category'].split(',')
+            products = products.filter(category__name__in=category)
+            category = Category.objects.filter(name__in=category)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You need to enter a search value.")
+                return redirect(reverse('home'))
+
+            queries = Q(product_name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'serach_term': query,
+        'current_category': category,
     }
 
     return render(request, 'home/index.html', context)
