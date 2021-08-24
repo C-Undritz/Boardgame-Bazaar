e9280c5218e86d33b815ecd57c59715d6624c54d
C-Undritz <undritz.cu@gmail.com>
2021-08-24 20:34:41 +0000

    feat: added code and required functions for 'used', 'on_sale' and 'new' queries

--- home/views.py
+++ home/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from products.models import Product, Category
+from products.models import Product
+
+import datetime
+
+
+def determine_new_releases():
+    """
+    Determines whether product is a new release or not.  Queries whether
+    each products release date is within the last previous period of months
+    (determined by the 'new_release_month_period' value) and updates the
+    product new_release boolean value accordingly.
+    """
+    new_release_month_period = 2
+    products = Product.objects.all()
+
+    def determine_past_date(value):
+        return value.replace(month=value.month - new_release_month_period)
+
+    today = datetime.date.today()
+    past_date = determine_past_date(today)
+
+    for product in products:
+        if (product.release_date > past_date) and (product.release_date < today):
+            this_product = product
+            this_product.new_release = True
+            this_product.save()
+            print("true")
+        else:
+            this_product = product
+            this_product.new_release = False
+            this_product.save()
+            print("False")
+
+# functions run when site starts
+determine_new_releases()
 
 
 def index(request):
@@ -12,14 +46,26 @@ def index(request):
     products = Product.objects.all()
     query = None
     category = None
+    page_heading = "Best Sellers"
 
-    # Search query function from Boutique Ado walkthrough project
     if request.GET:
-        if 'category' in request.GET:
-            category = request.GET['category'].split(',')
-            products = products.filter(category__name__in=category)
-            category = Category.objects.filter(name__in=category)
+        if 'used' in request.GET:
+            print('used requested')
+            page_heading = 'Used Games'
+            products = products.filter(used=True)
 
+        if 'on_sale' in request.GET:
+            print('on sale requested')
+            page_heading = 'Currently On Sale!'
+            products = products.filter(on_sale=True)
+
+        if 'new' in request.GET:
+            determine_new_releases()
+            print('new releases requested')
+            page_heading = 'Latest Releases!'
+            products = products.filter(new_release=True)
+
+        # Search query function from Boutique Ado walkthrough project
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -31,8 +77,9 @@ def index(request):
 
     context = {
         'products': products,
-        'serach_term': query,
+        'search_term': query,
         'current_category': category,
+        'heading': page_heading,
     }
 
     return render(request, 'home/index.html', context)
