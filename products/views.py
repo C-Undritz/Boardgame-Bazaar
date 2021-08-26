06bf5de1d81f2ba6f9117e4cb32b171c36d43289
C-Undritz <undritz.cu@gmail.com>
2021-08-26 21:51:55 +0000

    feat: added variables required to display genres

--- products/views.py
+++ products/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Genre, GenreAssignment
 
 
 def product_detail(request, product_id):
     """
     View to show individual product details
     """
+    genres = Genre.objects.all()
     product = get_object_or_404(Product, pk=product_id)
+    display_genres = GenreAssignment.objects.filter(product=product)
 
     context = {
         'product': product,
+        'genres': genres,
+        'display_genres': display_genres,
     }
 
     return render(request, 'products/product_detail.html', context)
