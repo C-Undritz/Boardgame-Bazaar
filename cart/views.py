91982598191cf708779714bb345d7f98f611a03e
C-Undritz <undritz.cu@gmail.com>
2021-09-01 08:11:23 +0000

    feat: added update cart functionality

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from products.models import Genre
 
 
@@ -37,3 +37,24 @@ def add_to_cart(request, item_id):
     request.session['cart'] = cart
     print(request.session['cart'])
     return redirect(redirect_url)
+
+
+def update_cart(request, item_id):
+    """
+    Updates the quantity of the specified product to a specified amount.
+    """
+    quantity = int(request.POST.get('quantity'))
+    print(f'the item quantity is: {quantity}')
+    # gets session 'cart' variable or creates it
+    cart = request.session.get('cart', {})
+    print(f'The cart contents is: {cart}')
+
+    # determines if item exists and updates quantity or adds item.
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop[item_id]
+
+    # overwrites the variable in the session with updated version
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
