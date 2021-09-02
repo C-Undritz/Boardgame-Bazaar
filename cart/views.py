24bb0a2398217b080be803aacd9a2e13fb22d877
C-Undritz <undritz.cu@gmail.com>
2021-09-02 16:34:31 +0000

    feat: added messages against add, update and remove from cart functions

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Genre, Product
 
@@ -19,7 +19,7 @@ def add_to_cart(request, item_id):
     Add a quantity of a product to the shopping cart.  Adapted from the CI
     Boutique Ado project
     """
-    product = Product.objects.get(pk=item_id)  # retrieved for messages
+    product = get_object_or_404(Product, pk=item_id)  # retrieved for messages
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -28,6 +28,7 @@ def add_to_cart(request, item_id):
     # determines if item exists and updates quantity or adds item.
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'You now have {cart[item_id]} copies of {product.product_name} in your cart')
     else:
         cart[item_id] = quantity
         messages.success(request, f'Added {product.product_name} to shopping cart')
@@ -42,6 +43,8 @@ def update_cart(request, item_id):
     Updates the quantity of the specified product to a specified amount.
     Adapted from the CI Boutique Ado project
     """
+    product = get_object_or_404(Product, pk=item_id)
+
     quantity = int(request.POST.get('quantity'))
     print(f'the item quantity is: {quantity}')
     cart = request.session.get('cart', {})
@@ -49,9 +52,11 @@ def update_cart(request, item_id):
     if quantity > 0:
         cart[item_id] = quantity
         print(f'update cart - the cart is: {cart}')
+        messages.success(request, f'{product.product_name} quantity updated to {cart[item_id]}')
     else:
         cart.pop(item_id)
         print(f'the cart is: {cart}')
+        messages.success(request, f'{product.product_name} removed from cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -59,16 +64,19 @@ def update_cart(request, item_id):
 
 def remove_from_cart(request, item_id):
     """
-    Removes the item from the shopping cart. Adapted from the CI Boutique Ado 
+    Removes the item from the shopping cart. Adapted from the CI Boutique Ado
     project.
     """
     try:
+        product = get_object_or_404(Product, pk=item_id)
         cart = request.session.get('cart', {})
         cart.pop(item_id)
+        messages.success(request, f'{product.product_name} removed from cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         print(e)
         return HttpResponse(status=500)
