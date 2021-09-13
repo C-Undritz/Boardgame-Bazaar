990382f9a1ffb00a080837ecbc103ea95a46937c
C-Undritz <undritz.cu@gmail.com>
2021-09-13 20:03:52 +0000

    feat: started construstion of profile menu and profile page

---
+++ profiles/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        # Fields to be rendered in the form.
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Overides init method Add placeholders and classes, remove
+        auto-generated labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)  # default method to set up the form.
+        # dictionary of placeholders that will show in the form fields.
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_town_or_city': 'Town or City',
+            'default_county': 'County',
+            'default_postcode': 'Postal Code',
+        }
+
+        # ensures cursor starts in full name field
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+
+        # interate through fields to apply the below:
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    # adds a star to required fields
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                # Sets the placeholder content to value defined in dictionary
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            # removes form labels
+            self.fields[field].label = False
+            self.fields[field].widget.attrs['class'] = 'profile-form-input'
--- profiles/views.py
+++ profiles/views.py
@@ -2,17 +2,25 @@ from django.shortcuts import render, get_object_or_404
 
 from products.models import Genre
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """
-    Displays the user profile page
+    Displays the user profile page.  
     """
     genres = Genre.objects.all()
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    form = UserProfileForm(instance=profile)
+
+    # gets users orders so order history can be displayed
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
         'genres': genres,
+        'form': form,
         'profile': profile,
     }
 
