72556a8a612dd04029ab9d95e02913f0a666d0ad
C-Undritz <undritz.cu@gmail.com>
2021-09-13 18:02:35 +0000

    feat: implemented and tested link to and display of profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from products.models import Genre
+from .models import UserProfile
 
 
 def profile(request):
     """
     Displays the user profile page
     """
+    genres = Genre.objects.all()
+    profile = get_object_or_404(UserProfile, user=request.user)
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'genres': genres,
+        'profile': profile,
+    }
 
     return render(request, template, context)
