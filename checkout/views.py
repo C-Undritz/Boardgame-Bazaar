f97681e1300766ae7e76540b0588aef686ae0006
C-Undritz <undritz.cu@gmail.com>
2021-09-14 14:42:32 +0000

    feat: saved delivery info will now auto fill checkout form

--- checkout/views.py
+++ checkout/views.py
@@ -10,6 +10,8 @@ from django.http import JsonResponse
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 from cart.contexts import cart_contents
 
 import stripe
@@ -152,7 +154,28 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        order_form = OrderForm()
+        # pre-fill delivery information if saved in profile:
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    # full name and email from user account
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    # remainder is information from user profile
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'town_or_city': profile.default_town_or_city,
+                    'county': profile.default_county,
+                    'postcode': profile.default_postcode,
+                    'country': profile.default_country,
+                    'phone_number': profile.default_phone_number,
+                })
+                # if user not authenticated an empty form is rendered
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing')
@@ -173,8 +196,29 @@ def checkout_success(request, order_number):
     """
     # saves to a variable, the users choice to save form information to profile
     save_info = request.session.get('save_info')
-
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the users profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # save the user's info
+        if save_info:
+            profile_data = {
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_town_or_city': order.town_or_city,
+                'default_county': order.county, 
+                'default_postcode': order.postcode,
+                'default_country': order.country,
+                'default_phone_number': order.phone_number,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(request, f'Order successful.  Your order \
         number is {order_number}. A confirmation email will be \
         sent to {order.email}.')
