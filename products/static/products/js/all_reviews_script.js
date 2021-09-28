2f25d0a0a2b014a0be6c2708349a0b8299ff011b
C-Undritz <undritz.cu@gmail.com>
2021-09-28 13:40:48 +0000

    feat: required Javascript to display more than the three reviews shown as standard

---
+++ products/static/products/js/all_reviews_script.js
@@ -0,0 +1,35 @@
+/* 
+<----------  Below functions are associated with the product_detail.html page ---------->
+*/
+
+let revealReviews = document.getElementById('reveal-all-reviews');
+let hideReviews = document.getElementById('hide-all-reviews');
+let allReviewsText  = document.getElementById('all-reviews');
+
+/* Event Listeners for the display of the reviews.  The display of features depends on the amount of reviews 
+against the product.  Therefore the 'if' statements test for when the elements are included in the html before 
+determining how they are displayed.*/
+document.addEventListener("DOMContentLoaded", function () {
+    if ((allReviewsText) && (hideReviews)) {
+        allReviewsText.style.display = "none";
+        hideReviews.style.display = "none";
+    }
+});
+
+// Determines what is displayed in the reviews section of a product detail view when the text 'See all reviews' is clicked.
+if (revealReviews) { 
+    revealReviews.addEventListener('click', function () {
+        revealReviews.style.display = "none";
+        hideReviews.style.display = "block";
+        allReviewsText.style.display = "block";
+    });
+}
+
+// Determines what is displayed in the reviews section of a product detail view when the text 'Close' is clicked.
+if (hideReviews) {
+    hideReviews.addEventListener('click', function() {
+        revealReviews.style.display = "block";
+        hideReviews.style.display = "none";
+        allReviewsText.style.display = "none";
+    });
+}
