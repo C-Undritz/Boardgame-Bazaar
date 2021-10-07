1ad4276e8d00706ff533fb207a1b05e96c2a21fa
C-Undritz <undritz.cu@gmail.com>
2021-10-07 12:25:37 +0000

    fix: fixed bug where no item quantity causes error and sorted PEP8 issues

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
-from products.models import Genre, Product
+from products.models import Product
 
 
 def view_cart(request):
@@ -11,49 +11,45 @@ def view_cart(request):
 
 def add_to_cart(request, item_id):
     """
-    Add a quantity of a product to the shopping cart.  Adapted from the CI
-    Boutique Ado project
+    Add a quantity of a product to the shopping cart.  A bootstrap off-canvas
+    is used to display the cart updates to the customer whilst shopping.
+    Adapted from the CI Boutique Ado project
     """
-    product = get_object_or_404(Product, pk=item_id)  # retrieved for messages
-
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    cart = request.session.get('cart', {})  # gets session 'cart' variable or creates it
-    SHOP = 50
+    cart = request.session.get('cart', {})
 
-    # determines if item exists and updates quantity or adds item.
+    # Determines if item exists and updates quantity or adds item.
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
-        # messages.success(request, f'You now have {cart[item_id]} copies of {product.product_name} in your cart')
-        messages.add_message(request, SHOP, 'test')
+        messages.add_message(request, 50, 'value')
     else:
         cart[item_id] = quantity
-        # messages.success(request, f'Added {product.product_name} to shopping cart')
-        messages.add_message(request, SHOP, 'test')
+        messages.add_message(request, 50, 'value')
 
-    # overwrites the variable in the session with updated version
+    # Overwrites the cart variable in the session with updated version.
     request.session['cart'] = cart
     return redirect(redirect_url)
 
 
 def update_cart(request, item_id):
     """
-    Updates the quantity of the specified product to a specified amount.
-    Adapted from the CI Boutique Ado project
+    Updates the quantity of the specified product to a specified amount. User
+    should not be able to enter a '0' quantity at any point.  Adapted from
+    the CI Boutique Ado project.
     """
     product = get_object_or_404(Product, pk=item_id)
-
-    quantity = int(request.POST.get('quantity'))
-    # print(f'the item quantity is: {quantity}')
+    quantity = int(request.POST.get('quantity') or 1)
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
-        # print(f'update cart - the cart is: {cart}')
-        messages.success(request, f'{product.product_name} quantity updated to {cart[item_id]}')
+        messages.success(
+            request,
+            f'{product.product_name} quantity updated to {cart[item_id]}')
     else:
+        # Should not be needed; kept in incase scenario not perceived occurs.
         cart.pop(item_id)
-        # print(f'the cart is: {cart}')
         messages.success(request, f'{product.product_name} removed from cart')
 
     request.session['cart'] = cart
@@ -76,5 +72,4 @@ def remove_from_cart(request, item_id):
 
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
-        # print(e)
         return HttpResponse(status=500)
