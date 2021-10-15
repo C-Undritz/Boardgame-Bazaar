24fafb526b25b3ed96593bafcc0b8847ae00775a
C-Undritz <undritz.cu@gmail.com>
2021-10-15 18:00:31 +0000

    feat: added mailinglist app, models and admin complete

---
+++ mailinglist/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import MailingList
+
+
+class MailingListAdmin(admin.ModelAdmin):
+    list_display = (
+        'email',
+        'date_added',
+    )
+
+    ordering = ('date_added',)
+
+
+admin.site.register(MailingList, MailingListAdmin)
