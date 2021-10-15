027f259947dcfc12b6553dfd4ce2e6e0a8cf0485
C-Undritz <undritz.cu@gmail.com>
2021-10-15 20:09:17 +0000

    feat: added mailing list functionality to home app

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+# from .models import MailingList
 
-# Register your models here.
+
+# class MailingListAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'email',
+#         'date_added',
+#     )
+
+#     ordering = ('date_added',)
+
+
+# admin.site.register(MailingList, MailingListAdmin)
--- home/views.py
+++ home/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+# from .models import MailingList
+from django.contrib import messages
+# from .forms import AddToMailingList
 
 
 def index(request):
-    """ A view that renders the shopping cart page """
+    """
+    Displays the home page. Post function adds a customer
+    entered email to the mailing list.
+    """
+    # if request.method == 'POST':
+    #     mailing_list = MailingList.objects.all()
+    #     print(f'mailing list is: {mailing_list}')
+    #     form_data = {
+    #         'email': request.POST['email'],
+    #     }
+    #     print(f'the email sudmitted is: {form_data}')
+    #     form = AddToMailingList(form_data)
+    #     if form.is_valid():
+    #         form.save()
+    #         messages.success(request, 'Email address added to mailing list')
+    #         return redirect(reverse('home'))
+    #     else:
+    #         messages.error(request, 'Failed to add email. Please check that you have correctly filled out all required information.')
+    #         return redirect(reverse('home'))
 
     return render(request, 'home/index.html')
