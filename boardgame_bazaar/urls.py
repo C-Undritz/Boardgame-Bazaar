e0fc5751e9a7c1faf61ccc54053e225acb221767
C-Undritz <undritz.cu@gmail.com>
2021-10-16 15:58:39 +0000

    feat: added mailing_list app urls

--- boardgame_bazaar/urls.py
+++ boardgame_bazaar/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('mailing_list/', include('mailing_list.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
