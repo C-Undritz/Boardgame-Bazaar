af948cba6867a4aa7c0999936ec1b76eca9cd640
C-Undritz <undritz.cu@gmail.com>
2021-10-16 11:53:44 +0000

    maint: removing mailing list functionality from home app

--- home/models.py
+++ home/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 
-class MailingList(models.Model):
-    email = models.EmailField(null=False, blank=False)
-    date_added = models.DateField(auto_now_add=True)
+# class MailingList(models.Model):
+#     email = models.EmailField(null=False, blank=False)
+#     date_added = models.DateField(auto_now_add=True)
 
-    def __str__(self):
-        return self.email
+#     def __str__(self):
+#         return self.email
--- home/views.py
+++ home/views.py
@@ -1,28 +1,8 @@
-from django.shortcuts import render, redirect, reverse
-from .models import MailingList
-from django.contrib import messages
-from .forms import AddToMailingList
+from django.shortcuts import render
 
 
 def index(request):
     """
-    Displays the home page. Post function adds a customer
-    entered email to the mailing list.
+    Displays the home page.
     """
-    if request.method == 'POST':
-        mailing_list = MailingList.objects.all()
-        print(f'mailing list is: {mailing_list}')
-        form_data = {
-            'email': request.POST['email'],
-        }
-        print(f'the email sudmitted is: {form_data}')
-        form = AddToMailingList(form_data)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Email address added to mailing list')
-            return redirect(reverse('home'))
-        else:
-            messages.error(request, 'Failed to add email. Please check that you have correctly filled out all required information.')
-            return redirect(reverse('home'))
-
     return render(request, 'home/index.html')
