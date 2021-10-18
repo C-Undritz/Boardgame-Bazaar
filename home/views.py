e0c7cc597f0c393470076c1b6cb33e9273be3f78
C-Undritz <undritz.cu@gmail.com>
2021-10-18 20:47:03 +0000

    feat: added information page for links from footer

--- home/views.py
+++ home/views.py
@@ -6,3 +6,10 @@ def index(request):
     Displays the home page.
     """
     return render(request, 'home/index.html')
+
+
+def information(request):
+    """
+    Displays the information page.
+    """
+    return render(request, 'home/information.html')
