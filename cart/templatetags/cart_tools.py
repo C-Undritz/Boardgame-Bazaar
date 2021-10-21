dd44cc4d11fc0a1820536a09303555e7f8d7b863
C-Undritz <undritz.cu@gmail.com>
2021-10-21 16:59:52 +0000

    maint: added doc strings

--- cart/templatetags/cart_tools.py
+++ cart/templatetags/cart_tools.py
@@ -1,8 +1,17 @@
+"""
+Boardgame Bazaar: cart App - cart_tools
+"""
+
+
 from django import template
 
 
 register = template.Library()
 
+
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
+    """
+    Calculates the subtotal to be displayed in the cart and checkout pages.
+    """
     return price * quantity
