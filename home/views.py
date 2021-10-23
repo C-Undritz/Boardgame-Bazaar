adf3c0902fb08b3c98a14ccb2947192d447c615e
C-Undritz <undritz.cu@gmail.com>
2021-10-23 16:20:18 +0000

    feat: new views for each information page linked from footer

--- home/views.py
+++ home/views.py
@@ -13,8 +13,43 @@ def index(request):
     return render(request, 'home/index.html')
 
 
-def information(request):
+def aboutus(request):
     """
-    Displays the information page.
+    Displays the about us information page.
     """
-    return render(request, 'home/information.html')
+    return render(request, 'home/aboutus.html')
+
+
+def shipping(request):
+    """
+    Displays the shipping information page.
+    """
+    return render(request, 'home/shipping.html')
+
+
+def returns(request):
+    """
+    Displays the returns information page.
+    """
+    return render(request, 'home/returns.html')
+
+
+def tandcs(request):
+    """
+    Displays the terms & conditions information page.
+    """
+    return render(request, 'home/tandcs.html')
+
+
+def careers(request):
+    """
+    Displays the careers information page.
+    """
+    return render(request, 'home/careers.html')
+
+
+def privacy(request):
+    """
+    Displays the privacy policy information page.
+    """
+    return render(request, 'home/privacy.html')
